8061eb30f837868a6a9bb8567e9641c8e3045ed2
ReconElement <omkarpanda895@gmail.com>
2025-01-04 22:14:36 +0530

    Build added
    Build added

--- src/assets/buttons.tsx
+++ src/assets/buttons.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react';
+// import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch, { SwitchProps } from '@mui/material/Switch';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
+// import FormGroup from '@mui/material/FormGroup';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+// import Stack from '@mui/material/Stack';
+// import Typography from '@mui/material/Typography';
 
 export const MaterialUISwitch = styled(Switch)(({ theme }) => ({
     width: 62,
